711b9e4cdfceeda81dbe3b513adb8807c7af65fe
itsreimau <private.iblmln@gmail.com>
2024-12-08 16:47:21 +0700

    [Fix] Removed unnecessary JavaScript logic and updated HTML links to improve page functionality and user experience.

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,6 @@
-// Function to show the specified section and hide others
-function showSection(sectionId) {
-    const sections = document.querySelectorAll(".dynamic-section");
-    sections.forEach((section) => (section.style.display = "none"));
-    document.getElementById(sectionId).style.display = "block";
-}
-
 // Preloader
 window.onload = function() {
     setTimeout(function() {
         document.getElementById("preloader").style.display = "none";
     }, 2000);
 };
-
-// Automatically show the About Me section when the page loads
-window.addEventListener("load", function() {
-    showSection("aboutMe");
-});
